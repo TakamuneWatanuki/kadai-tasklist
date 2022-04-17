6f6910966663bd20c6d61519d65df98360fc7282
takamune.watanuki <tenshinw@outlook.com>
2022-04-17 07:51:36 +0000

    create user and login

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  
+  has_many :tasks
+end
